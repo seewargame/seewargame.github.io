a63ce478c74231c218157de2b939b5c5a96ae5df
seewargame <40520211+seewargame@users.noreply.github.com>
2019-10-25 15:57:39 +0400

    Add files via upload

---
+++ precache-manifest.200a55eb8a49eafb5401a3efcf176b34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a02c09d9a17f362ba2d80c148fe823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15ff8fba77334481a792",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "3c787a54b8509a78bdec",
+    "url": "/static/js/2.d36cfe7f.chunk.js"
+  },
+  {
+    "revision": "15ff8fba77334481a792",
+    "url": "/static/js/main.ea021925.chunk.js"
+  },
+  {
+    "revision": "e7563785cc716b01a009",
+    "url": "/static/js/runtime-main.73d44457.js"
+  }
+]);
